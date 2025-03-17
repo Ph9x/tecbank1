4778510d9473f91262f8e82fe2c5dc35f6d7c791
Ph9x <pedrohenriquesousa9010@gmail.com>
2025-03-17 10:13:38 -0300

    ADD ESTRUTURA

---
+++ app/index.tsx
@@ -0,0 +1,17 @@
+import { View, Text, StyleSheet } from "react-native";
+
+function App() {
+    return (
+        <View>
+            <Text>PEdro</Text>
+        </View>
+    );
+}
+
+const style = StyleSheet.create({
+    container: {
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#00f"
+    }
+});
